190beab43531a96a17b0936566f1bed0af48c2ed
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-01 12:03:15 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,38 @@ public class PracticeProblem {
 
 	}
 
+
+	public static int goodString(String string) {
+		int f = string.length();
+
+		//good string check
+		if (f >= 2 && string.charAt(0) == string.charAt(f - 1)) {
+			return 0;
+		}
+
+		if (f < 2) {
+			return -1;
+		}
+
+		int smallOpp = Integer.MAX_VALUE;
+
+		//try to make goodstring
+		for (int i = 0; i < f; i++) {
+			for (int j = f - 1; j>= i; j--) {
+				if (i < j && string.charAt(i) == string.charAt(j)) {
+					int opp = i + (f - 1 - j);
+
+					smallOpp = Math.min(smallOpp, opp);
+				}
+			}
+
+		}
+
+		//no goodstring can be formed
+		if (smallOpp == Integer.MAX_VALUE) {
+			return -1;
+		}
+
+		return smallOpp;
+    }
 }
